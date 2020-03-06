b94f40b066b7d242bfbf63d641f3619b942720b3
Chudier <chudierp@gmail.com>
2020-03-06 09:30:14 -0800

    added templates and are rendered on root page

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import *
 
 # Create your views here.
 
 def index(request):
-    context = {}
-    return render(request, 'todo/index.html', context)
+    tasks = Task.objects.all()
+
+    context = {'tasks':tasks}
+    return render(request,'todo/index.html', context)
