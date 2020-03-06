10d7d8199aa154b26ec7575d1766773e79ddc214
Chudier <chudierp@gmail.com>
2020-03-06 10:10:20 -0800

    added edit option

--- todo/urls.py
+++ todo/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('update/<str:pk>/', views.updateTask, name='update'),
 ]
+ 
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import *
@@ -10,6 +10,18 @@ def index(request):
     tasks = Task.objects.all()
 
     form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')   
+
     
     context = {'tasks':tasks, 'form':form}
     return render(request,'todo/index.html', context)
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+
+    return render(request, 'todo/update.html')    
