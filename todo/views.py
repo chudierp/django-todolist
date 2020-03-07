1066191f39bfd85bb9f6ec5b4ec36c2b56dcdff6
Chudier <chudierp@gmail.com>
2020-03-06 22:07:01 -0600

    completed need to add css next

--- todo/views.py
+++ todo/views.py
@@ -35,3 +35,13 @@ def updateTask(request, pk):
     context = {'form': form}
 
     return render(request, 'todo/update.html', context)    
+
+def deleteTask(request, pk):
+    item = Task.objects.get(id=pk)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')  
+
+    context = {'item':item}
+    return render(request, 'todo/delete.html', context)
